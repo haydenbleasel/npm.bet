b256cae74415ba1693ed9215b74462662394f29d
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-13 21:53:48 -0800

    Dynamic metadata title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,29 @@ import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Main } from "@/components/main";
 
-const Home = () => (
+type HomeProps = {
+  searchParams: Promise<{
+    q: string;
+  }>;
+};
+
+export const generateMetadata = async ({ searchParams }: HomeProps) => {
+  const { q } = await searchParams;
+
+  if (!q) {
+    return {
+      title: "npm.bet",
+    };
+  }
+
+  const packages = q.split(",");
+
+  return {
+    title: `${packages.join(" vs ")} - npm.bet`,
+  };
+};
+
+const Home = async () => (
   <div className="grid h-dvh grid-rows-[2rem_1fr_2rem] gap-4 overflow-hidden p-4">
     <Header />
     <Main />
